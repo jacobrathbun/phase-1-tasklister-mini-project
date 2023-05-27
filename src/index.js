994bf958fdd9963c05118f58379a1ca14a315774
Jacob Rathbun <jrathofbun@gmail.com>
2023-05-27 00:03:54 -0400

    Complete Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//adds functionality to the submit form and resets
 document.addEventListener("DOMContentLoaded", () => {
   let form = document.querySelector('form')
   form.addEventListener('submit', (e) => {
@@ -7,6 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
   })
 });
 
+//builds a task item in the list and adds a functioning delete button
 function buildTask(task) {
   console.log(task)
   let p = document.createElement('p')
@@ -18,6 +20,7 @@ function buildTask(task) {
   document.querySelector('#list').appendChild(p)
 }
 
+//handles the "delete" event by pressing the X button
 function handleDelete(e) {
   e.target.parentNode.remove()
 }
